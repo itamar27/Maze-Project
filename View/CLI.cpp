4fcc34085311f7b4a6925f46fdceb0a74dcd9fab
Itamar Yarden <itamary9@gmail.com>
2020-08-25 11:18:13 +0300

    View is updated

--- View/CLI.cpp
+++ View/CLI.cpp
@@ -12,40 +12,45 @@ void CLI::start()
     std::vector<std::string> parameters;
     while (command.compare("exit") != 0)
     {
-        _out << ">>";
-        std::getline(_in, command);
-
-        parameters = parseInput(command, controller);
-        Command *com = controller->get(command);
-
-        if (nullptr != com)
+        std::string command = "";
+        MyController *controller = new MyController(this);
+        std::vector<std::string> parameters;
+        while (command.compare("exit") != 0)
         {
-            try
-            {
-                com->execute(parameters);
-            }
-            catch (const char *e)
-            {
-                std::cout << e << std::endl;
-            }
-        }
-        else
-            _out << "Unsupported Command!" << std::endl;
-    }
-    delete controller;
-}
+            _out << ">>";
+            std::getline(_in, command);
 
-/*
+            parameters = parseInput(command, controller);
+            Command *com = controller->get(command);
+
+            if (nullptr != com)
+            {
+                try
+                {
+                    com->execute(parameters);
+                }
+                catch (const char *e)
+                {
+                    std::cout << e << std::endl;
+                }
+            }
+            else
+                _out << "Unsupported Command!" << std::endl;
+        }
+        delete controller;
+    }
+
+    /*
  *      Method: getOstream()
  *      Description: A friend function to return the 'out' stream of view to help other MVC members 
  */
 
-std::ostream &CLI::getOStream()
-{
-    return _out;
-}
+    std::ostream &CLI::getOStream()
+    {
+        return _out;
+    }
 
-/* 
+    /* 
  *      Method: parseInput()
  *      Description: This method is parsing the string into a command and a vector that includes 
  */
@@ -54,31 +59,35 @@ std::vector<std::string> CLI::parseInput(std::string &str, MyController *con)
 {
     std::vector<std::string> vec;
     std::string command = "";
-    std::string tmp = "";
+    std::string strCopy = str;
     std::string delimiter = " ";
     size_t pos = 0;
 
-    while (pos = str.find(delimiter) != std::string::npos)
+    while (strCopy.length() > 0) // loop for parsing the string into vector where each element is a single word.
     {
-        vec.push_back(str.substr(0, pos));
-        str.erase(0, pos + delimiter.length());
+        pos = strCopy.find(delimiter);
+        vec.push_back(strCopy.substr(0, pos));
+        if (pos != std::string::npos)
+            strCopy.erase(0, pos + delimiter.length());
+
+        else
+            strCopy.erase(0, pos);
     }
 
+    std::string commandCopy = "";
     int commandParse = 0;
-    for (int i = 0; i < vec.size(); ++i)
+    for (int i = 0; i < vec.size(); ++i) // loop for finding the largest command in string
     {
-        command += vec[i];
-        if (con->get(command))
-            commandParse = i;
+        commandCopy += vec[i];
+        if (con->get(commandCopy))
+        {
+            command = commandCopy;
+            commandParse = i+1;
+        }
+        commandCopy += " ";
     }
 
-    command = vec[0];
-    for (int i = 1; i < commandParse; i++)
-    {
-        command += delimiter + vec[i];
-    }
-
-    vec.erase(vec.begin(), vec.begin() + commandParse);
+    vec.erase(vec.begin(), (vec.begin() + commandParse));
 
     str = command;
     return vec;
